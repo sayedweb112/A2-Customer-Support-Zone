bd8792850de4373993a639f3ac3882a50b4f6a64
unknown <112sayedalsahab@gmail.com>
2026-03-05 16:28:57 +0600

    added navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
 import './App.css'
-
+import Navbar from './components/Navbar';
 function App() {
   
 
   return (
     <>
   
-      <h1>Vite + React</h1>
+     {/* Navbar */}
+      <Navbar />
     
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-white dark:bg-gray-900 shadow-md px-6">
+
+      {/* Logo */}
+      <div className="flex-1">
+        <a className="text-xl font-bold text-gray-800 dark:text-white">
+          CS – Ticket System
+        </a>
+      </div>
+
+      {/* Menu */}
+      <div className="flex-none hidden md:flex gap-6 items-center">
+        <a className="text-gray-700 dark:text-gray-300 hover:text-primary cursor-pointer">
+          Home
+        </a>
+
+        <a className="text-gray-700 dark:text-gray-300 hover:text-primary cursor-pointer">
+          FAQ
+        </a>
+
+        <a className="text-gray-700 dark:text-gray-300 hover:text-primary cursor-pointer">
+          Changelog
+        </a>
+
+        <a className="text-gray-700 dark:text-gray-300 hover:text-primary cursor-pointer">
+          Blog
+        </a>
+
+        <a className="text-gray-700 dark:text-gray-300 hover:text-primary cursor-pointer">
+          Contact
+        </a>
+
+        <button className="btn btn-primary">
+          + New Ticket
+        </button>
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
